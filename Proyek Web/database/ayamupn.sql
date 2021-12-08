e2e2c5cb9069712147c9b9f5d1516ed578ad39a0
ryzalfadhillah <94983291+ryzalfadhillah@users.noreply.github.com>
2021-12-08 10:23:09 +0700

    Add files via upload

--- Proyek Web/database/ayamupn.sql
+++ Proyek Web/database/ayamupn.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 24 Nov 2021 pada 17.21
+-- Waktu pembuatan: 08 Des 2021 pada 04.22
 -- Versi server: 10.4.14-MariaDB
 -- Versi PHP: 7.4.11
 
@@ -24,14 +24,34 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `detail_transaksi`
+-- Struktur dari tabel `keranjang`
 --
 
-CREATE TABLE `detail_transaksi` (
-  `id_detail` int(10) NOT NULL,
-  `id_transaksi` int(10) NOT NULL,
-  `id_produk` int(10) NOT NULL,
-  `qty` int(20) NOT NULL
+CREATE TABLE `keranjang` (
+  `id_keranjang` int(10) NOT NULL,
+  `produk_id` int(10) NOT NULL,
+  `user` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `keranjang`
+--
+
+INSERT INTO `keranjang` (`id_keranjang`, `produk_id`, `user`) VALUES
+(5, 12, 'user1'),
+(6, 6, 'user1'),
+(16, 7, 'user1');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `pesanan`
+--
+
+CREATE TABLE `pesanan` (
+  `id_pesanan` int(10) NOT NULL,
+  `total` int(20) NOT NULL,
+  `tanggal` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -54,26 +74,13 @@ CREATE TABLE `produk` (
 
 INSERT INTO `produk` (`produk_id`, `nama_produk`, `harga`, `gambar`, `kategori`) VALUES
 (3, 'Burger + Kentang', 30000, 'blog-3.jpg', 'Makanan'),
-(4, 'Kopi Moca', 12000, 'cat-6.png', 'Minuman'),
 (5, 'Kentang', 8000, 'banner-2.png', 'Makanan'),
 (6, 'Pizza', 40000, 'food-3.png', 'Makanan'),
 (7, 'Burger', 20000, 'food-2.png', 'Makanan'),
 (8, 'Hotdog', 20000, 'menu-4.png', 'Makanan'),
 (11, 'Steak Daging', 40000, 'steak.jpeg', 'Makanan'),
-(12, 'Air Mineral', 3500, 'air mineral.jpeg', 'Minuman');
-
--- --------------------------------------------------------
-
---
--- Struktur dari tabel `transaksi`
---
-
-CREATE TABLE `transaksi` (
-  `id_transaksi` int(10) NOT NULL,
-  `user_id` int(10) NOT NULL,
-  `tanggal` date NOT NULL DEFAULT current_timestamp(),
-  `sub_total` int(10) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+(12, 'Air Mineral', 3500, 'air mineral.jpeg', 'Minuman'),
+(13, 'Kopi Moca', 12000, 'cat-6.png', 'Minuman');
 
 -- --------------------------------------------------------
 
@@ -96,32 +103,31 @@ CREATE TABLE `users` (
 INSERT INTO `users` (`user_id`, `username`, `nama_lengkap`, `password`, `status`) VALUES
 (1, 'admin', 'admin', 'admin', 'admin'),
 (2, 'user1', 'user1', 'user1', 'user'),
-(3, 'user2', 'user2', 'user2', 'user');
+(3, 'user2', 'user2', 'user2', 'user'),
+(9, 'Ryzal', 'Ryzal', 'Ryzal', 'user'),
+(10, 'user3', 'user3', 'user3', 'user');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indeks untuk tabel `detail_transaksi`
+-- Indeks untuk tabel `keranjang`
 --
-ALTER TABLE `detail_transaksi`
-  ADD PRIMARY KEY (`id_detail`),
-  ADD UNIQUE KEY `id_transaksi` (`id_transaksi`),
-  ADD UNIQUE KEY `id_produk` (`id_produk`);
+ALTER TABLE `keranjang`
+  ADD PRIMARY KEY (`id_keranjang`);
+
+--
+-- Indeks untuk tabel `pesanan`
+--
+ALTER TABLE `pesanan`
+  ADD PRIMARY KEY (`id_pesanan`);
 
 --
 -- Indeks untuk tabel `produk`
 --
 ALTER TABLE `produk`
   ADD PRIMARY KEY (`produk_id`);
-
---
--- Indeks untuk tabel `transaksi`
---
-ALTER TABLE `transaksi`
-  ADD PRIMARY KEY (`id_transaksi`),
-  ADD KEY `user_id` (`user_id`);
 
 --
 -- Indeks untuk tabel `users`
@@ -135,45 +141,28 @@ ALTER TABLE `users`
 --
 
 --
--- AUTO_INCREMENT untuk tabel `detail_transaksi`
+-- AUTO_INCREMENT untuk tabel `keranjang`
 --
-ALTER TABLE `detail_transaksi`
-  MODIFY `id_detail` int(10) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `keranjang`
+  MODIFY `id_keranjang` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
+
+--
+-- AUTO_INCREMENT untuk tabel `pesanan`
+--
+ALTER TABLE `pesanan`
+  MODIFY `id_pesanan` int(10) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT untuk tabel `produk`
 --
 ALTER TABLE `produk`
-  MODIFY `produk_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
-
---
--- AUTO_INCREMENT untuk tabel `transaksi`
---
-ALTER TABLE `transaksi`
-  MODIFY `id_transaksi` int(10) NOT NULL AUTO_INCREMENT;
+  MODIFY `produk_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT untuk tabel `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
-
---
--- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
---
-
---
--- Ketidakleluasaan untuk tabel `detail_transaksi`
---
-ALTER TABLE `detail_transaksi`
-  ADD CONSTRAINT `detail_transaksi_ibfk_1` FOREIGN KEY (`id_produk`) REFERENCES `produk` (`produk_id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `detail_transaksi_ibfk_2` FOREIGN KEY (`id_transaksi`) REFERENCES `transaksi` (`id_transaksi`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Ketidakleluasaan untuk tabel `transaksi`
---
-ALTER TABLE `transaksi`
-  ADD CONSTRAINT `transaksi_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  MODIFY `user_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
